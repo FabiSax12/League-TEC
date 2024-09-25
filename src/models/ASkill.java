39cfb4001acfeb094e05742c85bcc4828cf32f4a
Fabián Vargas <fabidev18@gmail.com>
2024-09-24 21:11:50 -0600

    Creacin de las primeras clases y planteamientos

--- src/models/ASkill.java
+++ src/models/ASkill.java
@@ -1,6 +1,12 @@
 package models;
 
 public abstract class AAbility {
+    String name;
+
+    public AAbility(String name){
+        this.name = name;
+    }
+
     public abstract void use();
     public abstract void getName();
     public abstract int getManaCost();
--- src/models/CharacterFactory.java
+++
@@ -1,7 +0,0 @@
-package models;
-
-public abstract class CharacterFactory {
-    public static ACharacter createCharacter(Element type){
-        return null;
-    }
-}
